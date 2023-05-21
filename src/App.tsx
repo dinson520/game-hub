5fdb425c87a039e2e5ccc1e482aef6b600bb92f2
dinson520 <dinson520@gmail.com>
2023-05-21 16:05:18 +0800

    Build a responsive layout used Chakra Grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "asign main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          NAV
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="asign" bg="gold">
+            ASIGN
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          MAIN
+        </GridItem>
+      </Grid>
     </>
   );
 }
