321a421ac2bb4089c1028a0631429318e5bd7095
dinson520 <dinson520@gmail.com>
2023-05-28 13:37:55 +0800

    Add error case.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,14 +4,15 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 
 const PlatformSelector = () => {
-  const { data: platforms } = usePlatforms();
+  const { data, error } = usePlatforms();
+  if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         Platforms
       </MenuButton>
       <MenuList>
-        {platforms.map((platform) => (
+        {data.map((platform) => (
           <MenuItem key={platform.id}>{platform.name}</MenuItem>
         ))}
       </MenuList>
